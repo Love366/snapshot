ffaa2b1ea512b9ad85bbae397f2efcf26c7b682c
Lovepreet Singh <lovepreet.singh28@live.vu.edu.au>
2020-10-11 13:25:57 +0000

    Initial Commit

---
+++ webapp/controller/Feedback.controller.js
@@ -0,0 +1,224 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.untitledPrototype.controller.Feedback", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5f822d4cc517dc3da4e27e23";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onToggleButtonPress: function() {
+			return new Promise(function(fnResolve) {
+				var sTargetPos = "center center";
+				sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
+				sap.m.MessageToast.show("FEED BACK GIVEN", {
+					onClose: fnResolve,
+					duration: 3000 || 3000,
+					at: sTargetPos,
+					my: sTargetPos
+				});
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Products", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		applyFiltersAndSorters: function(sControlId, sAggregationName, chartBindingInfo) {
+			if (chartBindingInfo) {
+				var oBindingInfo = chartBindingInfo;
+			} else {
+				var oBindingInfo = this.getView().byId(sControlId).getBindingInfo(sAggregationName);
+			}
+			var oBindingOptions = this.updateBindingOptions(sControlId);
+			this.getView().byId(sControlId).bindAggregation(sAggregationName, {
+				model: oBindingInfo.model,
+				path: oBindingInfo.path,
+				parameters: oBindingInfo.parameters,
+				template: oBindingInfo.template,
+				templateShareable: true,
+				sorter: oBindingOptions.sorters,
+				filters: oBindingOptions.filters
+			});
+
+		},
+		updateBindingOptions: function(sCollectionId, oBindingData, sSourceId) {
+			this.mBindingOptions = this.mBindingOptions || {};
+			this.mBindingOptions[sCollectionId] = this.mBindingOptions[sCollectionId] || {};
+
+			var aSorters = this.mBindingOptions[sCollectionId].sorters;
+			var aGroupby = this.mBindingOptions[sCollectionId].groupby;
+
+			// If there is no oBindingData parameter, we just need the processed filters and sorters from this function
+			if (oBindingData) {
+				if (oBindingData.sorters) {
+					aSorters = oBindingData.sorters;
+				}
+				if (oBindingData.groupby || oBindingData.groupby === null) {
+					aGroupby = oBindingData.groupby;
+				}
+				// 1) Update the filters map for the given collection and source
+				this.mBindingOptions[sCollectionId].sorters = aSorters;
+				this.mBindingOptions[sCollectionId].groupby = aGroupby;
+				this.mBindingOptions[sCollectionId].filters = this.mBindingOptions[sCollectionId].filters || {};
+				this.mBindingOptions[sCollectionId].filters[sSourceId] = oBindingData.filters || [];
+			}
+
+			// 2) Reapply all the filters and sorters
+			var aFilters = [];
+			for (var key in this.mBindingOptions[sCollectionId].filters) {
+				aFilters = aFilters.concat(this.mBindingOptions[sCollectionId].filters[key]);
+			}
+
+			// Add the groupby first in the sorters array
+			if (aGroupby) {
+				aSorters = aSorters ? aGroupby.concat(aSorters) : aGroupby;
+			}
+
+			var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, true)] : undefined;
+			return {
+				filters: aFinalFilters,
+				sorters: aSorters
+			};
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Feedback").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+			var oView = this.getView(),
+				oData = {},
+				self = this;
+			var oModel = new sap.ui.model.json.JSONModel();
+			oView.setModel(oModel, "staticDataModel");
+			self.oBindingParameters = {};
+
+			oData["sap_m_Page_0-content-sap_ui_richtexteditor_RichTextEditor-1602368505784"] = {};
+
+			oView.getModel("staticDataModel").setData(oData, true);
+
+			function dateDimensionFormatter(oDimensionValue, sTextValue) {
+				var oValueToFormat = sTextValue !== undefined ? sTextValue : oDimensionValue;
+				if (oValueToFormat instanceof Date) {
+					var oFormat = sap.ui.core.format.DateFormat.getDateInstance({
+						style: "short"
+					});
+					return oFormat.format(oValueToFormat);
+				}
+				return oValueToFormat;
+			}
+
+		},
+		onAfterRendering: function() {
+
+			var oChart,
+				self = this,
+				oBindingParameters = this.oBindingParameters,
+				oView = this.getView();
+
+		}
+	});
+}, /* bExport= */ true);
